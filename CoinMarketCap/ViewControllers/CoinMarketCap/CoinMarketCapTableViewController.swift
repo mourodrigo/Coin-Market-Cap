2d6cc8daac0380dd6b05f60fafda0a4d7170c975
mourodrigo <rodrigobt20@gmail.com>
2018-03-21 20:31:31 -0300

    Tableview implementation using realm datasource

--- CoinMarketCap/ViewControllers/CoinMarketCap/CoinMarketCapTableViewController.swift
+++ CoinMarketCap/ViewControllers/CoinMarketCap/CoinMarketCapTableViewController.swift
@@ -7,18 +7,16 @@
 //
 
 import UIKit
+import RealmSwift
 
 class CoinMarketCapTableViewController: UITableViewController {
+
+    let realm = try! Realm()
+    let defaultCoin = "USD $"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         TickerClient.init().fetch()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,27 +25,34 @@ class CoinMarketCapTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+    
+    func dataSource() -> Results<Coin> {
+        return realm.objects(Coin.self).sorted(byKeyPath: "market_cap_usd", ascending: false)
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return dataSource().count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CoinMarketCapTableViewCell",
+                                                       for: indexPath) as? CoinMarketCapTableViewCell
+            else { fatalError("CoinMarketCapTableViewCell class implementation could not be found") }
 
-        // Configure the cell...
+        let coin = dataSource()[indexPath.row]
+        
+        cell.coinNameLabel.text = coin.name
+        cell.marketCapLabel.text = "\(defaultCoin) \(coin.market_cap_usd)"
+        cell.priceLabel.text = "\(defaultCoin) \(coin.price_usd)"
 
         return cell
     }
-    */
-
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
