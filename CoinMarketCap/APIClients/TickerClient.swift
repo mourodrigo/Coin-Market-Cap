907a5756e386b236783f6c89f8ae761c2b880b76
mourodrigo <rodrigobt20@gmail.com>
2018-03-21 20:05:53 -0300

    Implemented api response json mapping to Coin model. Persisting all coin occurrences on storage using transaction for speed improvements.

--- CoinMarketCap/APIClients/TickerClient.swift
+++ CoinMarketCap/APIClients/TickerClient.swift
@@ -21,7 +21,34 @@ class TickerClient: Object {
         Alamofire.request(url).responseSwiftyJSON(queue: DispatchQueue.global(),
                                                   options: JSONSerialization.ReadingOptions.mutableLeaves)
         { response in
-            print(response);
+            
+            if let items = response.value?.array {
+                
+                let realm = try! Realm()
+                
+                if !realm.isInWriteTransaction { realm.beginWrite() }
+                
+                for item in items {
+                    let coin = Coin(id: item["id"].stringValue,
+                                    name: item["name"].stringValue,
+                                    symbol: item["symbol"].stringValue,
+                                    rank: item["rank"].intValue,
+                                    price_usd: item["price_usd"].doubleValue,
+                                    price_btc: item["price_btc"].doubleValue,
+                                    volume_usd_24: item["24h_volume_usd"].doubleValue,
+                                    market_cap_usd: item["market_cap_usd"].int64Value,
+                                    available_supply: item["available_supply"].doubleValue,
+                                    total_supply: item["total_supply"].doubleValue,
+                                    max_supply: item["max_supply"].doubleValue,
+                                    percent_change_1h: item["percent_change_1h"].doubleValue,
+                                    percent_change_24h: item["percent_change_24h"].doubleValue,
+                                    percent_change_7d: item["percent_change_7d"].doubleValue,
+                                    last_updated: Date(timeIntervalSince1970: item["last_updated"].doubleValue)
+                    )
+                    realm.add(coin, update: true)
+                }
+                if realm.isInWriteTransaction { try! realm.commitWrite() }
+            }
         }
     }
     
